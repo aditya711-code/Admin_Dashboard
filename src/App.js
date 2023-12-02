3240f09ef35f1ba580cec8c8b42a27353c8b62e0
Aditya Mane <adityamane711@gmail.com>
2023-12-02 16:00:50 +0530

    Reverted change in package.json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Admin from "./Admin";
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.css";
